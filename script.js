f76eac80baf05738fe5abe342e820bf0ba6135ba
Jordi Hoven <hovenjordi@gmail.com>
2018-10-18 17:34:45 +0200

    Add vibes page with animations

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
-	$('.preloader-background')
-	.delay(4000)
+	$('.preloader')
+	.delay(2000)
 	.fadeOut('slow');
 
-	$('.preloader-background h1')
-	.delay(3000)
+	$('.preloader h1')
+	.delay(1000)
 	.fadeOut('slow');
-	
-	// $('.preloader-wrapper')
-	// 	.delay(1500)
-	// 	.fadeOut();
 });
