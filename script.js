b5a05047ec647cf2c0000d50da1a073c1329b0ad
Jordakoes <hovenjordi@gmail.com>
2018-10-18 16:05:39 +0200

    Loading screen

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+	$('.preloader-background')
+		.delay(2000)
+		.fadeOut('slow');
+	
+	$('.preloader-wrapper')
+		.delay(2000)
+		.fadeOut();
+});
