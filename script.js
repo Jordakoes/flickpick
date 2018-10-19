b359701ab6395a10dc2b3e3e3bc8e9602f0bc96f
Jordakoes <hovenjordi@gmail.com>
2018-10-19 08:43:06 +0200

    Add preloader to generate page + clean up code

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
 	$('.preloader')
 	.delay(2000)
-	.fadeOut('slow');
+	.fadeOut('slow')
 
 	$('.preloader h1')
 	.delay(1000)
 	.fadeOut('slow');
+
 });
+
+//trying to get the scrollbar during preloader to hide...
+//document.body.style.overflow = 'hidden';
+//document.getElementById("test").style.overflow = 'hidden';
